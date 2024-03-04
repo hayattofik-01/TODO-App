45c8034478d8a280589fa7553e41feeda9a4e552
Hayat Tofik <hayattofik22@gmail.com>
2024-03-05 02:51:51 +0300

    add appwrite auth

---
+++ todo_app/lib/app/data/provider/appwrite_provider.dart
@@ -0,0 +1,18 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as models;
+
+class AppWriteProvider{
+  Client client = Client();
+  Account? account;
+  AppWriteProvider(){
+    client.setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject("65e56e8009b0e8613eeb")
+    .setSelfSigned(status: true);
+    account = Account(client);
+  }
+    Future<models.User> signup(Map map) async {
+
+    final response = account!.create(userId: map["userId"], email:  map["email"], password:  map["password"],name: map["name"]);
+    return response;
+  }
+}
---
+++ todo_app/lib/app/data/repository/auth_repository.dart
@@ -0,0 +1,9 @@
+import 'package:todo_app/app/data/provider/appwrite_provider.dart';
+import 'package:appwrite/models.dart' as models;
+
+class AuthRepository{
+  final AppWriteProvider appWriteProvider;
+  AuthRepository(this.appWriteProvider);
+
+  Future<models.User> signup(Map map) => appWriteProvider.signup(map);
+}
